73437f8dde0f4100f3f2b548865375721755822d
KonstantinKirillOff <53314883+KonstantinKirillOff@users.noreply.github.com>
2021-08-23 12:53:55 +0300

    add logic

--- TConverter/ViewController.swift
+++ TConverter/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var celsiusLabel: UILabel!
+    @IBOutlet weak var forengateLabel: UILabel!
+    @IBOutlet weak var slider: UISlider! {
+        didSet {
+            slider.maximumValue = 100
+            slider.minimumValue = 0
+            slider.value = 0
+        }
     }
+    
 
-
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        let tempretureCelsius = Int(round(sender.value))
+        celsiusLabel.text = "\(tempretureCelsius)ºC"
+        
+        let fahrenheitTemperature = Int(round((sender.value * 9 / 5) + 32))
+        forengateLabel.text = "\(fahrenheitTemperature)ºF"
+    }
+    
 }
 
